b4a32f391d995bbd0e0883adbba3b4fd83357587
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-02-05 17:53:10 -0500

    Update code_from _book.hs

--- Part 1/code_from _book.hs
+++ Part 1/code_from _book.hs
@@ -1,6 +1,12 @@
 
+-- Chapter 1: Category
+
 Prelude> :t (.)
 (.) :: (b -> c) -> (a -> b) -> a -> c
 
 -- Not from book
 map (1+) . reverse
+
+-- Chapter 2: Types & Functions
+
+fact n = product [1 .. n]
