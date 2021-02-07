1ae3b7b793b2bca49395acdd163172c23292a1e1
Shon Feder <shon.feder@gmail.com>
2021-02-07 15:56:57 -0500

    Correct comment

--- Part 1/Chapter 2 - Types and Functions/shon_solutions.ml
+++ Part 1/Chapter 2 - Types and Functions/shon_solutions.ml
@@ -19,9 +19,9 @@ let memo f =
     match Hashtbl.find_opt ht x with
     | Some y -> y
     | None   ->
-        let y = f x in
-        Hashtbl.add ht x y;
-        y
+      let y = f x in
+      Hashtbl.add ht x y;
+      y
 
 let%expect_test "memo memoizes (morally) pure functions" =
   let f x =
@@ -51,8 +51,8 @@ let%test "memoization of pure functions" =
         ~name:"memoized function are equal to the function"
         (pair (fun1 Observable.string int) string)
         (fun (f, x) ->
-          let memo_f = memo (Fn.apply f) in
-          Fn.apply f x = memo_f x)
+           let memo_f = memo (Fn.apply f) in
+           Fn.apply f x = memo_f x)
     ]
 
 (** Exercise 2
@@ -67,16 +67,16 @@ let%test "memoization of non-deterministic functions" =
         ~name:"Random.int gives different results on repeated calls"
         (int_range 100 10000)
         (fun bound ->
-          assume (bound > 0);
-          Random.int bound <> Random.int bound)
+           assume (bound > 0);
+           Random.int bound <> Random.int bound)
     ; Test.make
         ~name:
           "memoized Random.int doesn't give different results on repeated \
            calls "
         (int_range 100 10000)
         (fun bound ->
-          let memo_rand = memo Random.int in
-          not (memo_rand bound <> memo_rand bound))
+           let memo_rand = memo Random.int in
+           not (memo_rand bound <> memo_rand bound))
     ]
 
 (** Exercise 3
@@ -94,9 +94,9 @@ let%test _ =
         ~name:"init_rand is equal to its memoized version"
         int
         (fun seed ->
-          let memo_init_rand = memo init_rand in
-          init_rand seed = memo_init_rand seed
-          && init_rand seed = memo_init_rand seed)
+           let memo_init_rand = memo init_rand in
+           init_rand seed = memo_init_rand seed
+           && init_rand seed = memo_init_rand seed)
     ]
 
 (** Exercise 4
@@ -165,12 +165,7 @@ module Ex_6 = struct
 
   (** Here we explicitly enumerate the morphisms represented in the diagram
       (but also remember to include the endomorphisms), according each object
-      it's own namespace.
-
-      For each submodule:
-
-      - [t] is the type of the object
-      - ['a arr] is a morphisms from the object to a target object ['a] *)
+      it's own namespace. *)
 
   (** {4 Morphisms from unit }*)
   module rec Unit : (sig
