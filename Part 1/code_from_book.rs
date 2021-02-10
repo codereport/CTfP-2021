51726df2ea76749d20625f19a7310aab87b40f66
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-02-10 00:58:06 -0500

    Update code_from_book.rs

--- Part 1/code_from_book.rs
+++ Part 1/code_from_book.rs
@@ -6,3 +6,15 @@
 fn fact(n: i32) -> i32 {
     (1..=n).product()
 }
+
+// Chapter 3: Categories Great and Small
+
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=423070c111fa47c3ec4a8b5f62bde454
+
+fn sum(n: i32) -> i32 {
+    (1..=n).fold(0, |a, b| a + b )
+}
+
+fn product(n: i32) -> i32 {
+    (1..=n).fold(1, |a, b| a * b )
+}
