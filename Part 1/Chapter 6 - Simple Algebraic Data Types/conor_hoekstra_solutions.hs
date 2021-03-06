90683621f42cbf9e5ae39338d508d96f1ac4a27b
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-03-06 12:44:23 -0500

    Update conor_hoekstra_solutions.hs

--- Part 1/Chapter 6 - Simple Algebraic Data Types/conor_hoekstra_solutions.hs
+++ Part 1/Chapter 6 - Simple Algebraic Data Types/conor_hoekstra_solutions.hs
@@ -12,3 +12,18 @@ main :: IO ()
 main = do
     print $ maybeToEither (Just 42)    -- Right 42
     print $ eitherToMaybe (Right 1729) -- Just 1729
+
+-- Question 5
+
+eitherToPair :: Either a a -> (Bool, a)
+eitherToPair (Left a)  = (False, a)
+eitherToPair (Right a) = (True, a)
+
+pairToEither :: (Bool, a) -> Either a a
+pairToEither (False, a) = Left a
+pairToEither (True, a)  = Right a
+
+main :: IO ()
+main = do
+    print $ eitherToPair (Left 42)    -- (False, 42)
+    print $ pairToEither (True, 1729) -- Right 1729
